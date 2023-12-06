fecb789ef75bde14a209411921b2347575178dcf
Vieirinhagui <vieiragui147@gmail.com>
2023-12-06 20:12:14 -0300

    login ajuste

--- assets/js/logar.js
+++ assets/js/logar.js
@@ -19,23 +19,34 @@ function logar() {
     password: password,
   };
 
-  fetch("https://vps50387.publiccloud.com.br:3333/login", {
+  fetch("https://191.252.205.138:3333/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   })
-    .then((response) => response.json())
-    .then((responseData) => {
-      if (responseData.token) {
-        sessionStorage.setItem("token", responseData.token);
+    .then((response) => {
+      // Verifica se a resposta é bem-sucedida
+      if (!response.ok) {
+        throw new Error("Falha na resposta da rede");
+      }
+
+      // Extrai o token do cabeçalho 'Authorization'
+      const token = response.headers.get("Authorization");
+
+      // Verifica se o token existe
+      if (token) {
+        // Armazena o token na sessionStorage
+        sessionStorage.setItem("token", token.split(" ")[1]); // Assume que o formato é 'Bearer token'
         window.location.href = "./paginaInicial.html";
       } else {
-        alert("Login falhou. Verifique suas credenciais.");
+        alert("Login falhou. Token não encontrado no cabeçalho.");
       }
     })
     .catch((error) => {
+      // Trata erros de rede ou de processamento
       console.error("Ocorreu um erro durante o login:", error);
+      alert("Erro ao realizar o login.");
     });
 }
